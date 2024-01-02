0655a61d329b6ea215e3e0b498469f6c34dcb874
asthaagarwal01 <astha2agarwal5@gmail.com>
2024-01-02 17:38:45 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import Logo from '../Assets/images/Logo.png';
+import search from '../Assets/images/search.png';
+import login from '../Assets/images/login.png';
+import cart from '../Assets/images/cart.png';
+import './Header.css';
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div className='header'>
+            <div className='logo'>
+                <img src={Logo} alt='Logo' />
+            </div>
+
+            <div className='search'>
+                <input type='text' placeholder='Lets find your pair' />
+                <button><img src={search} alt='search' /></button>
+            </div>
+
+            <div className='second'>
+                <div className='nav'>
+                    <ul>
+                        <li><a href="#">Become a partner</a></li>
+                        <li><a href="#">Find More</a></li>
+                    </ul>
+                </div>
+
+                <div className='login'>
+                    <button>Login<img src={login} alt='login' /></button>
+                </div>
+
+                <div className='cart'>
+                    <button><img src={cart} alt='cart' />Cart</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
